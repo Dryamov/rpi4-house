cffb52b8901d4083e9dfeee55b955047fa7d698b
dryamov <dryamov.dev@gmail.com>
2020-04-07 20:49:36 +0000

    Before build

--- scripts/rebuild-all.sh
+++ scripts/rebuild-all.sh
@@ -3,7 +3,7 @@ git submodule update --rebase
 git add .
 git commit -m "Before build"
 git push
-git submodule foreach makepkg --clean --ignorearch --ignorearch --syncdeps
+git submodule foreach makepkg --clean --ignorearch --ignorearch --syncdeps --force
 cp ~/packages/*.pkg.tar.xz packages/
 repo-add --remove --new  rpi4-house.db.tar.gz  packages/*.pkg.tar.xz
 rclone sync   ./  google_drive:/Fast.io/rpi4-house.imfast.io --progress --copy-links  --exclude-from configs/exclude-sync.conf
