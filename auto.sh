db885f6d22fea794948a66882e7b448a278ff061
dryamov <dryamov.dev@gmail.com>
2020-04-07 19:48:50 +0000

    r

--- auto.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#rclone mount --vfs-cache-mode writes --daemon  google_drive:Fast.io/aarch64.imfast.io/packages ../packages
-git submodule update --rebase
-git submodule foreach makepkg --clean --ignorearch --ignorearch --syncdeps
-repo-add  --remove  ../packages/aarch64.db.tar.gz ../packages/*.pkg.tar.xz
-rclone sync  ../packages/  google_drive:/Fast.io/aarch64.imfast.io/packages --progress
-#fusermount -u  ../packages
